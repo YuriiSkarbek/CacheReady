bb1145ec3d75e1c1573d958069e0f241260528bb
yuriiskarbek <yurii.skarbek@gmail.com>
2021-02-04 15:33:09 +0200

    Added typewriter effect to intro block

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+// Typewriter for intro block
+var app = document.getElementById('intro-typing');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(500)
+  .typeString('Drupal')
+  .pauseFor(800)
+  .deleteChars(6)
+  .typeString('WordPress')
+  .pauseFor(800)
+  .deleteChars(9)
+  .typeString('Magento')
+  .pauseFor(800)
+  .deleteChars(7)
+  .typeString('Varnish')
+  .pauseFor(800)
+  .deleteChars(7)
+  .typeString('inner ;)')
+  .pauseFor(1200)
+  .start();
