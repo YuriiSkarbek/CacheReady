ccbc288250119fdd53b8d84cc92f6b2bbc1e4d53
yuriiskarbek <yurii.skarbek@gmail.com>
2021-02-07 17:46:59 +0200

    Added range slider for pricing

--- scripts/main.js
+++ scripts/main.js
@@ -32,3 +32,43 @@ typewriter
     });
   };
   
+
+  function rangeChange(val){
+    rangeChangePricing(val);
+    rangeChangeBackground(val);
+  }
+
+  function rangeChangePricing(val){
+    // make bold current value 
+    $('#rangeVal'+val).css('font-weight', 'bold');
+
+    // make default style for not current value
+    for(let i = 1; i < 7; i++){
+      if(i != val){
+        $('#rangeVal'+i).css('font-weight', 'normal');
+      } else {
+        console.log('Cuurent is: ', i);
+        if(i == 1 || i == 2){
+          $('.pricing-card').removeClass('active');
+          $('#pricingCard1').addClass('active');
+        }
+
+        if(i == 3 || i == 4){
+          $('.pricing-card').removeClass('active');
+          $('#pricingCard2').addClass('active');
+        }
+
+        if(i == 5 || i == 6){
+          $('.pricing-card').removeClass('active');
+          $('#pricingCard3').addClass('active');
+        }
+        
+      }
+    }
+  }
+
+  function rangeChangeBackground(val){
+    console.log('val: ', val);
+    $('.pricing-slider').removeClass('x1 x2 x3 x4 x5 x6')
+    $('.pricing-slider').addClass('x'+val);
+  }
